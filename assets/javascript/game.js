2243cac993af0b51e36ec76abf6f39ed80c0ae58
AJ Ford <AJCranford1019@gmail.com>
2017-03-06 21:24:34 -0500

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,4 @@
+alert("test");
 $(document).ready(function(){
 
 var searchInput = $('#searchInput').val();
@@ -5,16 +6,16 @@ var startYear = $('#startYear').val();
 var endYear = $('#endYear').val();
 
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
-var apiKey = "apikey=6389bcb7860f4fc8800665bce1a81bd0"
-var q = 'q' + searchInput; 
-var  startYear = '&' + 'begin_date=' + startYear + '0000';
-var endYear = '&' + 'end_date=' + endYear + '0000';
+var apiKey = "&apikey=6389bcb7860f4fc8800665bce1a81bd0"
+var q = 'q=' + searchInput; 
+var  startYear = '&' + 'begin_date=' + startYear + '0101';
+var endYear = '&' + 'end_date=' + endYear + '0101';
 
-url + q + startYear + endYear + apiKey;
+var queryUrl = url + q + startYear + endYear + apiKey;
 
 
 $.ajax({
-  url: url,
+  url: queryUrl,
   method: 'GET',
 }).done(function(result) {
   console.log(result);
