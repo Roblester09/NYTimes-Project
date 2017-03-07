86cd2e61e50bc8b04998875d76e4301c92195459
AJ Ford <AJCranford1019@gmail.com>
2017-03-06 21:16:29 -0500

    js update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
 
-
+var searchInput = $('#searchInput').val();
+var startYear = $('#startYear').val();
+var endYear = $('#endYear').val();
 
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
 var apiKey = "apikey=6389bcb7860f4fc8800665bce1a81bd0"
-var q = 'q' + user form input; 
-var  startYear = '&' + 'begin_date=' + userinput + '0000';
-var endYear = '&' + 'end_date=' + userinput + '0000';
+var q = 'q' + searchInput; 
+var  startYear = '&' + 'begin_date=' + startYear + '0000';
+var endYear = '&' + 'end_date=' + endYear + '0000';
 
 url + q + startYear + endYear + apiKey;
 
